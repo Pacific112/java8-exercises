e592caa7ba80097e476ceeec74afe4cf500518aa
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 18:58:08 +0100

    Streams

--- src/main/java/_4_streams/_1_api/StreamExample.java
+++ src/main/java/_4_streams/_1_api/StreamExample.java
@@ -1,7 +1,20 @@
 package _4_streams._1_api;
 
+import java.util.stream.IntStream;
+
 public class StreamExample {
 
     public static void main(String[] args) {
+        long count = IntStream.of(1, 2, 3).count();
+        long sum = IntStream.of(1, 2, 3).sum();
+        double average = IntStream.of(1, 2, 3).average().getAsDouble();
+
+        int mapSum = IntStream.of(1, 2, 3)
+                              .map(i -> i * 2)
+                              .sum();
+
+        long filterCount = IntStream.of(1, 2, 3, 4, 5)
+                                    .filter(i -> i > 2)
+                                    .count();
     }
 }
