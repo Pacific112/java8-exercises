f1bcc962f609fef8ee75e6078b882335c536115a
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 17:42:48 +0100

    Exercise 2 solved

--- src/main/java/_2_date/_2_exercise/ApiExercise.java
+++ src/main/java/_2_date/_2_exercise/ApiExercise.java
@@ -1,5 +1,9 @@
 package _2_date._2_exercise;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Month;
+
 public class ApiExercise {
 
     // TODO: exercise 1 - LocalDate API
@@ -9,9 +13,21 @@ public class ApiExercise {
     //       4. Od utworzonej daty odejmij ilość dni równą rokowi Twojego urodzenia
     //       5. Po dodaniu sprawdź jaki jest to miesiąc, czy jest to weekend oraz czy jest to rok przestępny
     public static void main(String[] args) {
+        LocalDate birthday = LocalDate.of(1992, Month.AUGUST, 25);
+        System.out.println(birthday.getDayOfWeek());
+        System.out.println(birthday.getDayOfYear());
 
+        LocalDate today = LocalDate.now();
+        LocalDate earlier = today.minusDays(birthday.getYear());
 
+        System.out.println(earlier.getMonth());
+        System.out.println(earlier.isLeapYear());
+
+        DayOfWeek dayOfWeek = earlier.getDayOfWeek();
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            System.out.println("It is weekend");
+        } else {
+            System.out.println("It is not weekend");
+        }
     }
-
-
 }
