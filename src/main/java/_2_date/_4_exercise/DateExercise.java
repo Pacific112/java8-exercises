ab86c48775e0555a6d26fcf8d961d7445eb1c825
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 18:27:00 +0100

    Exercise 3 introduced

--- src/main/java/_2_date/_4_exercise/DateExercise.java
+++ src/main/java/_2_date/_4_exercise/DateExercise.java
@@ -1,5 +1,11 @@
 package _2_date._4_exercise;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
 public class DateExercise {
 
     // TODO: Exercise 3 - Date Summary
@@ -8,6 +14,22 @@ public class DateExercise {
     //       Zakładamy że dni wolne od pracy to jedynie weekendy.
     //       Dla chętnych: wypisz na ekran informację ile minut pozostało do następnego dnia pracującego.
     public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
 
+        LocalDate workingDay = nextWorkingDay(now.toLocalDate());
+        System.out.println(workingDay.format(DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy")));
+
+        System.out.println("Do poniedziałku pozostało minut: " + ChronoUnit.MINUTES.between(now, workingDay.atStartOfDay()));
+    }
+
+    private static LocalDate nextWorkingDay(LocalDate today) {
+        if (today.getDayOfWeek() == DayOfWeek.SATURDAY) {
+            return today.plusDays(2);
+        }
+        if (today.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            return today.plusDays(1);
+        }
+
+        return today;
     }
 }
