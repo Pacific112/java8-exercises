07e38353748f3382325b3e3f6dc1b2fce27fe827
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 18:13:08 +0100

    Date formats introduced

--- src/main/java/_2_date/_1_api/DateExample.java
+++ src/main/java/_2_date/_1_api/DateExample.java
@@ -2,7 +2,9 @@ package _2_date._1_api;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateExample {
 
@@ -22,5 +24,11 @@ public class DateExample {
         System.out.println(twoMonthsLater.getDayOfWeek());
         System.out.println(twoMonthsLater.getMonth());
         System.out.println(twoMonthsLater.getDayOfMonth());
+
+        LocalDate parsedToday = LocalDate.parse("2019-03-10");
+        System.out.println(parsedToday);
+
+        LocalDate americanToday = LocalDate.parse("03-10-2019", DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+        System.out.println(americanToday.format(DateTimeFormatter.ofPattern("MMMM-dd-yy", Locale.GERMAN)));
     }
 }
