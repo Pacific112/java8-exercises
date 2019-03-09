f6efc7f6ce185e7bdc1451c795acce11a5574425
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 18:22:21 +0100

    LocalDateTime introduced

--- src/main/java/_2_date/_1_api/DateExample.java
+++ src/main/java/_2_date/_1_api/DateExample.java
@@ -1,8 +1,8 @@
 package _2_date._1_api;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.Locale;
 
@@ -30,5 +30,15 @@ public class DateExample {
 
         LocalDate americanToday = LocalDate.parse("03-10-2019", DateTimeFormatter.ofPattern("MM-dd-yyyy"));
         System.out.println(americanToday.format(DateTimeFormatter.ofPattern("MMMM-dd-yy", Locale.GERMAN)));
+
+        System.out.println(LocalTime.now());
+        System.out.println(LocalDateTime.now());
+        System.out.println(Instant.now().getEpochSecond());
+
+        LocalDate birthDate = LocalDate.of(1992, Month.AUGUST, 25);
+        LocalDateTime birthTime = birthDate.atTime(LocalTime.of(10, 20));
+        LocalDateTime timeNow = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.HOURS.between(birthTime, timeNow));
     }
 }
