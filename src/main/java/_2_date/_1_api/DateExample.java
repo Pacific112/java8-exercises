2ccf07c7dd121605bc24c5fec6a593d50c79d459
maciej.koziara <maciej.koziara@dac.digital>
2019-03-09 17:40:22 +0100

    Init commit

--- src/main/java/_2_date/_1_api/DateExample.java
+++ src/main/java/_2_date/_1_api/DateExample.java
@@ -1,7 +1,26 @@
 package _2_date._1_api;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
+
 public class DateExample {
 
     public static void main(String[] args) {
+        Date date = new Date(119, 2, 10);
+        System.out.println(date);
+
+        LocalDate today = LocalDate.of(2019, Month.MARCH, 10);
+        LocalDate now = LocalDate.now();
+
+        LocalDate twoMonthsLater = now.plusMonths(2);
+        System.out.println(twoMonthsLater);
+
+        LocalDate monthEarlier = today.minusDays(10);
+        System.out.println(monthEarlier);
+
+        System.out.println(twoMonthsLater.getDayOfWeek());
+        System.out.println(twoMonthsLater.getMonth());
+        System.out.println(twoMonthsLater.getDayOfMonth());
     }
 }
