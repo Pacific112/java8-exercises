7f672e43ff16437d98a47eee8ac6c290939054cb
maciej.koziara <maciej.koziara@airhelp.com>
2019-05-18 22:10:34 +0200

    Finding exercise solved

--- src/main/java/_4_streams/_2_finding/FindingStreamOperations.java
+++ src/main/java/_4_streams/_2_finding/FindingStreamOperations.java
@@ -3,8 +3,10 @@ package _4_streams._2_finding;
 import _4_streams.model.Dish;
 import _4_streams.model.Employee;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static _4_streams.model.Dish.menu;
 import static _4_streams.model.Employee.getEmployees;
@@ -19,28 +21,60 @@ public class FindingStreamOperations {
     public static void exercise1() {
         List<String> names = Arrays.asList("Adam", "Aleksandra", "Bartek", "Wiktoria", "Kacper", "Artur", "Stanisław",
                 "Jan", "Kacper", "Kalina", "Weronika", "Piotr", "Jeremiasz", "Zuzanna", "Kinga", "Alicja", "Michał");
+
+        Optional<String> result = names.stream()
+                .filter(n -> n.length() > 7)
+                .filter(n -> !n.startsWith("A"))
+                .findFirst();
+
+        System.out.println(result);
     }
 
     // TODO: Wypisz na ekran imię dowolnego pracownika powyżej 60 roku życia. Jeżeli taki pracownik nie istnieje wypisz na ekran: 'Brak takiego pracownika'.
     public static void exercise2() {
         List<Employee> employees = getEmployees();
 
+        String result = employees.stream()
+                .filter(e -> e.getAge() > 60)
+                .findFirst()
+                .map(Employee::getName)
+                .orElse("Brak takiego pracownika");
+
+        System.out.println(result);
     }
+
     // TODO: Wypisz na ekran imię dowolnego pracownika poniżej 20 roku życia. Jeżeli taki pracownik nie istnieje wypisz na ekran: 'Brak takiego pracownika'.
     public static void exercise3() {
         List<Employee> employees = getEmployees();
 
+        String result = employees.stream()
+                .filter(e -> e.getAge() < 20)
+                .findFirst()
+                .map(Employee::getName)
+                .orElse("Brak takiego pracownika");
+
+        System.out.println(result);
     }
 
     // TODO: Sprawdź czy któryś z pracowników zarabia powyżej 10000 zł
     public static void exercise4() {
         List<Employee> employees = getEmployees();
 
+        Optional<Employee> result = employees.stream()
+                .filter(e -> e.getSalary().compareTo(BigDecimal.valueOf(10_000)) > 0)
+                .findFirst();
+
+        System.out.println(result.isPresent());
     }
 
     // TODO: Upewnij się, że zadne z dań nie przekracza 1000 kalorii
     public static void exercise5() {
         List<Dish> menu = menu();
 
+        Optional<Dish> result = menu.stream()
+                .filter(d -> d.getCalories() > 1000)
+                .findFirst();
+
+        System.out.println(!result.isPresent());
     }
 }
